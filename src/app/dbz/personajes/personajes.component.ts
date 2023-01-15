1d0cb06f62ecdaf3b6d0d3584b517435950b8a60
Duvan Smith <dsmith.mr@gmail.com>
2023-01-15 10:55:16 -0500

    Padre hijo

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-personajes',
   templateUrl: './personajes.component.html',
 })
 export class PersonajesComponent {
-
+  @Input() personajes:any[]=[];
 }
