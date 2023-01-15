f5e360c841cb48afabe779b3a7618f083ccc1bf4
Duvan Smith <dsmith.mr@gmail.com>
2023-01-15 10:28:24 -0500

    Added ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
-  agregarEvent(event:any){
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 14000,
+  };
+
+  agregarEvent(event: any) {
     event.preventDefault();
-    console.log("pruebas con event")
+    console.log(this.nuevo);
   }
-  agregar(){
-    console.log("pruebas")
+
+  agregar() {
+    console.log(this.nuevo);
+  }
+
+  cambiarNombre(event: any) {
+    event.preventDefault();
+    console.log(event.target.value);
   }
 }
