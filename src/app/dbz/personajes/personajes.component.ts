50e143644c2381c3ba9364fa92bdf9b7b24a53e8
Duvan Smith <dsmith.mr@gmail.com>
2023-01-15 10:48:11 -0500

    Clean code

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-personajes',
   templateUrl: './personajes.component.html',
-  styleUrls: ['./personajes.component.css']
 })
 export class PersonajesComponent {
 
